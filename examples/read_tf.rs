51c43ec16bd82c61a16b44f0307bd65668e96935
erihsu <zhenyuxu163@163.com>
2021-02-04 19:54:04 +0800

    update

--- examples/read_tf.rs
+++ examples/read_tf.rs
@@ -1,16 +1,8 @@
-use std::fs::File;
-use std::io::Read;
 use std::time::*;
-use tf_parser::tf_parser;
+use tf_parser::parse_tf;
 fn main() {
-    let mut input_tf_file = File::open("scc40nll_hd_8lm_2tm.tf").expect("Invalid Directory");
-    let mut input_string = String::new();
-    input_tf_file.read_to_string(&mut input_string).unwrap();
-
-    println!("Load Tf...");
-
     let now = SystemTime::now();
-    let (_, _) = tf_parser(&input_string).unwrap();
+    let _ = parse_tf("scc40nll_hd_8lm_2tm.tf");
     let def2vulcan = SystemTime::now();
 
     println!(
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,25 @@
+//! The tf_parser is used to parse Technology(.tf) file, which is a part of [EDA](https://en.wikipedia.org/wiki/Electronic_design_automation) P&R tool.
+//! This file is usually provided by Foundary to describe basic technology information, technology layer, technology via, design rules, density rules, etc.
+
+//! # Example
+//!
+//!
+//!
+//!
+
 mod model;
 mod parser;
+pub use model::TfData;
 pub use parser::tf_parser::tf_parser;
+use std::fs;
+use std::path::Path;
+
+/// parse tf file into TfData struct
+pub fn parse_tf<P>(file_path: P) -> Result<TfData, Box<dyn std::error::Error>>
+where
+    P: AsRef<Path>,
+{
+    let input_string = fs::read_to_string(file_path)?;
+    let (_, result) = tf_parser(&input_string).unwrap();
+    Ok(result)
+}
